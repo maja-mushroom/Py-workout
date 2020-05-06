6c959d3f550ce38895bcf344714867fa99241902
unknown <majamandic32@yahoo.com>
2020-05-06 21:16:22 +0200

    gotov

--- news/news.py
+++ news/news.py
@@ -1,47 +1,83 @@
 import csv
 import random
 import os
+
 here = os.path.dirname(os.path.abspath(__file__))
 filename_i = os.path.join(here,"izlaz.txt")
-csv_ulaz = open ("ulaz.csv")
-csv_fraze = open ("phrases.csv")
+csv_ulaz = open("ulaz.csv")
+csv_fraze = open("phrases.csv")
 csv_fraze1 = csv.reader(csv_fraze, delimiter =",")
 with open("ulaz.csv") as f:
     reader = csv.reader(f)
     chosen_row = list(reader)
+    next(csv_ulaz)
 
+#ova funkcija nam sve fraze po tipu vraca u liste koje koristimo dalje za random odabir
+#ulazi nam lista fraza (csv_file)
+#izlazi nam lista fraza po tipu i slucajnom odabiru fraza
 
+def fraze_random(csv_fraze1):
+    fraze1 = []
+    fraze2 = []
+    fraze3 = []
 
-def phrases_append(csv_phrases):
-    phrase1 = []
-    phrase2 = []
-    phrase3 = []    
     for row1 in csv_fraze1:
         if row1[0] == "1":
-            phrase1.append(row1[1])
+            fraze1.append(row1[1])    
         if row1[0] == "2":
-            phrase2.append(row1[1])
+            fraze2.append(row1[1])    
         if row1[0] == "3":
-            phrase3.append(row1[1])
-    return (random.choice(phrase1),random.choice(phrase2),random.choice(phrase3))
-print(phrases_append(csv_fraze1))
+            fraze3.append(row1[1])
+    random1 = random.choice(fraze1)
+    random2 = random.choice(fraze2)
+    random3 = random.choice(fraze3)
+    return random1,random2,random3
+
+#ova funkacija nam vraca broj novozarazneih
+#ulazi nam lista sa ukupnim brojem zarazenih
+#izlaz je broj koji predstavlja novozarazene
+
+brojNovozarazenih = []
+def racuna(ukupnoZarazenih):
+    ukupnoZarazenih = [] 
+    for col in csv_ulaz:
+        ukupnoZarazenih.append(col[15:18])
+    for i in range(1,len(ukupnoZarazenih)):
+        x = int(ukupnoZarazenih[i]) - int(ukupnoZarazenih[i-1])
+        brojNovozarazenih.append(x)
+    broj = brojNovozarazenih[0]
+    return broj
 
 
-def chosen_num(phrase1,phrase2,phrase3,csv_ulaz1):
+#ova funkcija nam formatira tekset
+#ulaz je lista sa frazama (csv_file)
+#izlaz je lista sa frazama i brojem 
+
+def formatiranje(p,csv_ulaz):
     ulaz_values = [0]
     ulaz_values = chosen_row[1]
-    print (ulaz_values[1])
-    txt = phrase1.format(ulaz_values[1])
-    print(txt)
-    txt1 = phrase2.format(ulaz_values[2])
-    txt2 = phrase3.format(ulaz_values[3])
-    return(txt,txt1,txt2)
-print(chosen_num(phrase1,phrase2,phrase3,chosen_row))
 
+    fraze1 = p[0]
+    txt = fraze1.format(ulaz_values[1])
+    fraze2 = p[1]
+    txt1 = fraze2.format(abs(racuna(csv_ulaz)))
+    fraze3 = p[2]
+    txt2 = fraze3.format(ulaz_values[3])
+    return txt,txt1,txt2
+
+#ova funkcija ubacije u izlazni file fraze sa brojem
+#ulaz je csv_fie
+#izlaz je txt file
+
+def obradi(csv_fraze1):
+    p = fraze_random(csv_fraze1)
+    fraze = formatiranje(p,chosen_row)
+    p1,p2,p3 = fraze
+    return p1 + "\n" + p2 + "\n" + p3 + "\n"
 
 
 f_i = open(filename_i,'w')              
-f_i.write((line) + "\n")
-f_i.close(phrases_append(csv_fraze))
+f_i.write (obradi(csv_fraze1))
+f_i.close()
 csv_ulaz.close()
 csv_fraze.close()

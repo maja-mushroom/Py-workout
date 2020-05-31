9e677ec1f60369e3c912806fe5d3837b9f3177d3
B-real00 <biljica.popovic@gmail.com>
2020-05-31 20:57:13 +0200

    new function

--- banka/banka.py
+++ banka/banka.py
@@ -5,75 +5,113 @@ import csv
 print('\nWelcome, you have just entered in OT bank system!\n')
 print('\n')
 
-"""
-#pass check
-p = getpass.getpass(prompt='Enter your valid password? ')
-i=1
 
-#inp=0
-while i<=2:
-    if p.lower() == 'mica123':
-        print ('Welcome.\n')
-        inp = int(input('Otvaranje racuna - opcija 1\nStanje racuna 2\nUplata na racun - opcija 3\nIsplata sa racuna - opcija 4\nPrenos s jednog na drugi racuna - opcija 5\n'))
-        break
-    elif p.lower() == 'lazo123':
-        print ('Welcome.\n')
-        inp = int(input('Uplata na racun - opcija 1\nStanje racuna 2\nIsplata sa racuna - opcija 3\nPrenos s jednog na drugi racuna - opcija 4\nZatvaranje racuna - opcija 5\n'))
-        break
-    elif p.lower() == 'admin123':
-        print ('Welcome.\n')
-        inp = int(input('Otvaranje racuna - opcija 1\nStanje racuna 2\nUplata na racun - opcija 3\nIsplata sa racuna - opcija 4\nPrenos s jednog na drugi racuna - opcija 5\nZatvaranje racuna - opcija 6\n'))
-        break
-    else:
-        print ('Your password is not valid. Try again!\n')
-        p = getpass.getpass(prompt='Enter your valid password? ')
-        if (p.lower() != 'mica123') or (p.lower() != 'lazo123') or (p.lower() != 'admin123') :
-            print ('You are out of attempts. Your password is not valid.\n')
-            break
-    i+=1
+# #pass check
+# p = getpass.getpass(prompt='Enter your valid password? ')
+# i=1
+
+# #inp=0
+# while i<=2:
+#     if p.lower() == 'mica123':
+#         print ('Welcome.\n')
+#         inp = int(input('Otvaranje racuna - opcija 1\nStanje racuna - opcija 2\nUplata na racun - opcija 3\nIsplata sa racuna - opcija 4\nPrenos s jednog na drugi racuna - opcija 5\n'))
+#         break
+#     elif p.lower() == 'lazo123':
+#         print ('Welcome.\n')
+#         inp = int(input('Stanje racuna - opcija 2\nUplata na racun - opcija 3\nIsplata sa racuna - opcija 4\nPrenos s jednog na drugi racuna - opcija 5\nZatvaranje racuna - opcija 6\n'))
+#         break
+#     elif p.lower() == 'admin123':
+#         print ('Welcome.\n')
+#         inp = int(input('Otvaranje racuna - opcija 1\nStanje racuna - opcija 2\nUplata na racun - opcija 3\nIsplata sa racuna - opcija 4\nPrenos s jednog na drugi racuna - opcija 5\nZatvaranje racuna - opcija 6\n'))
+#         break
+#     else:
+#         print ('Your password is not valid. Try again!\n')
+#         p = getpass.getpass(prompt='Enter your valid password? ')
+#         if (p.lower() != 'mica123') or (p.lower() != 'lazo123') or (p.lower() != 'admin123'):
+#             print ('You are out of attempts. Your password is not valid.\n')
+#             break
+#     i+=1
 
 balance = 0
 payment = []
 payoff = []
 
 
+# def open_account():
+#     account_num = ''
+#     account_owner = ''
+#     balance = ''
+#     date = ''
 
-def open_account():
-    account_num = ''
-    account_owner = ''
-    date = ''
-    number = ''
-    balance = ''
+#     account_owner = input('Enter the user name and last name...\n')
+#     owner_jmgb = input('Enter the user JMBG: \n')
+#     account_num = input('Enter the user account number: \n')
+#     balance = input('Enter the user balance: \n')
+#     date = input('Enter the date od openig the account: \n')
 
-    account_num = input('Enter the user name and last name...\n')
-    account_owner = input('Enter the user JMBG: \n')
-    number = input('Enter the user account number: \n')
-    date = input('Enter the date od openig the account:\n')
-    balance = input('User balance: \n')
+#     with open('stanje.csv', "a") as csvfile:
+#         csvwriter = csv.writer(csvfile,  delimiter=',')
+#         csvwriter.writerow([account_owner, owner_jmgb, account_num , balance, date])
+#     return ("Points written sucessfully to file")
+# open_account()
+
+# def check_balance(account_n):
+#     balance = 0
+#     with open('stanje.csv','r') as csvfile:
+#         reader = csv.reader(csvfile,delimiter =',')
+#         for i in reader:
+#             #for j in i:
+#                 #print(j)
+#             if i[2] == str(account_n):
+#                 balance = i[-2]
+#         return balance   
+
+# print(check_balance(123456789))
 
 
-    with open('stanje.csv', "a", newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerow([account_num,account_owner,date,number,balance])
-    return ("Points written sucessfully to file")
-"""
-def check_balance(account_n):
-    balance = 0
-    with open('stanje.csv','r') as csvfile:
+# def deposit(balance):
+#     amount_deposit=float(input("The Amount: "))
+#     new_balance = balance + amount_deposit
+    
+#     print("New Amount: ", amount_deposit, '\n' "Total Blance:", new_balance)
+
+# deposit(900)
+
+# def withdraw(balance): 
+#     amount_withdraw = float(input("The Amount of withdraw: ")) 
+#     if balance >= amount_withdraw: 
+#         new_balance = balance - amount_withdraw
+#         print("You've just withdraw:", amount_withdraw) 
+#         print ("New Total is:" , new_balance)
+#     else: 
+#         print("There is no enough resources on the account for this transaction! ") 
+# print(withdraw(900))
+
+def transfer(account_of_withdraw, account_in_favour, amount_withdraw): 
+    new_balance = []
+    balance_append =[]
+    with open('stanje.csv','r+') as csvfile:
         reader = csv.reader(csvfile,delimiter =',')
         for i in reader:
-            for j in i:
-                #print(j)
-                if j == str(account_n):
-                    balance = i[-1]
-        return balance   
-print(check_balance(39394858483))
+            new_balance = i
+            if i[2] == str(account_of_withdraw):
+                balance = i[-2]
+                balance = int(balance) - amount_withdraw
+                new_balance[-2] = str(balance)
+                #with open('stanje.csv','a') as csvfile:
+                #csvwriter = csv.writer(csvfile, delimiter =',')
+                #csvwriter.writerows([new_balance])
+                print('new balance', balance)
+            #i = new_balance
+            #print(i)
+            if i[2] == str(account_in_favour):
+                balance = i[-2]
+                print(balance)
+                balance = int(balance) + amount_withdraw
+                balance_append[-2] = str(balance)
+                print('in favour', balance)
+        csvwriter = csv.writer(csvfile, delimiter =',')
+        csvwriter.writerows([new_balance])
+        csvwriter.writerows([balance_append])
 
-    
-
-if inp == 1:
-    open_account()
-    print('status OK')
-
-
-
+print(transfer(123456789, 123987654, 9))
